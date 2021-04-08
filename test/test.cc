fabab3ba81e77be1dab2f3a7e3f15eed004e458e
JIAWEI ZHANG <jz3209@columbia.edu>
2021-04-08 22:01:22 +0800

    add tests

--- test/test.cc
+++ test/test.cc
@@ -22,6 +22,29 @@ void square(int x) {
     std::cout << "acc: " << accum << std::endl;
 }
 
+
+
+void assertEqual(PaxosServiceImpl *p1, PaxosServiceImpl *p2, int seq) {
+  for (int i=1; i<=seq; i++) {
+    auto [ decided1, val1 ] = p1->Status(i);
+    auto [ decided2, val2 ] = p2->Status(i);
+    if (!decided1 || !decided2|| val1!=val2)
+      assert(false);
+  }
+}
+
+void assertFail(PaxosServiceImpl *p1, PaxosServiceImpl *p2, int seq) {
+  auto [ decided1, val1 ] = p1->Status(seq);
+  auto [ decided2, val2 ] = p2->Status(seq);
+  if (decided1 || decided2)
+    assert(false);
+}
+
+void shutdown_service(grpc::Server* server){
+  server->Shutdown();
+  std::cout << "wait for the server to shutdown..." << std::endl;
+}
+
 std::unique_ptr<grpc::Server> initialize_service(const std::string& server_address, grpc::Service* service)
 {
   grpc::ServerBuilder builder;
@@ -43,6 +66,7 @@ void start_service(grpc::Server* server)
   // wait for the server to shutdown
   server->Wait();
   std::cout << "wait for the server to shutdown..." << std::endl;
+
 }
 
 
@@ -145,17 +169,210 @@ void test_basic_put(){
   assert(decided_1 == decided_2 && val_1 == val_2 && "s1 and s2 agree");
   std::cout << "=============" << val_0 << " is decided among three servers\n";
 
+  // paxos_thread_0.join();
+  // paxos_thread_1.join();
+  // paxos_thread_2.join();
+  shutdown_service(paxos_server_0.get());
+  shutdown_service(paxos_server_1.get());
+  shutdown_service(paxos_server_2.get());
+  paxos_thread_0.join();
+  paxos_thread_1.join();
+  paxos_thread_2.join();
   return;
 
 }
 
+void test_unreliable() {
+  int replica_size = 5;
+  std::vector<std::unique_ptr<PaxosServiceImpl>> pxs;
+  std::vector<std::thread> pxs_threads;
+  std::vector<std::unique_ptr<grpc::Server>> servers;
+  std::vector<std::string> addr_v = {"0.0.0.0:50041", "0.0.0.0:50042", "0.0.0.0:50043",  "0.0.0.0:50044", "0.0.0.0:50045"};
+  std::vector<std::shared_ptr<grpc::Channel>> channels = make_channels(replica_size, addr_v);
+  
+  for (int i = 0; i < replica_size; ++i) {
+    std::cout << i << " start" << std::endl;
+    std::unique_ptr<PaxosServiceImpl> paxos_service = std::unique_ptr<PaxosServiceImpl>(
+      new PaxosServiceImpl(replica_size,channels, i)
+    );
+    std::unique_ptr<grpc::Server> paxos_server = initialize_service(addr_v[i], paxos_service.get());
+    std::thread paxos_thread(start_service, paxos_server.get()); // If not stored, paxos_server will be destructed every time after the loop
+    pxs_threads.push_back(std::move(paxos_thread));
+    pxs.push_back(std::move(paxos_service));
+    servers.push_back(std::move(paxos_server));
+    std::cout << i << " ok" << std::endl;
+  }
+
+
+
+  grpc::Status put_status = pxs.at(1)->Start(1,"put"); // use at as a reference
+  usleep(5000000);
+  put_status = pxs.at(2)->Start(2, "get");
+
+  pxs.at(1)->Start(3,"1");
+  pxs.at(1)->Start(4,"2");
+  assertEqual(pxs[0].get(),pxs[1].get(),4);
+  assertEqual(pxs[1].get(),pxs[2].get(),4);
+  assertEqual(pxs[2].get(),pxs[3].get(),4);
+  assertEqual(pxs[3].get(),pxs[4].get(),4);
+  std::cout << "pass test befor a few server die" << std::endl;
+
+  // make a minority of server die
+  std::set<int> deadServer = {3,4};
+  for (auto i : deadServer) {
+    shutdown_service(servers.at(i).get());
+  }
+  std::cout << "server 3 and server 4 is down" << std::endl;
+
+  pxs.at(0)->Start(5,"5");
+  pxs.at(1)->Start(6,"6");
+  assertEqual(pxs[0].get(),pxs[1].get(),6);
+  assertEqual(pxs[1].get(),pxs[2].get(),6);
+  std::cout << "alive servers function as expected" << std::endl;
+  // shut down alive the service
+  for (int i=0; i<replica_size; i++) {
+    if (deadServer.count(i)==0)
+      shutdown_service(servers.at(i).get());
+  }
+
+
+
+  // join all the threads
+  for (int i = 0; i < replica_size; i++) {
+    if (pxs_threads[i].joinable()) {
+      pxs_threads[i].join();
+      std::cout << i << " joined" << std::endl;
+    }
+  }
+}
+
+
+
+void test_minority() {
+  int replica_size = 5;
+  std::vector<std::unique_ptr<PaxosServiceImpl>> pxs;
+  std::vector<std::thread> pxs_threads;
+  std::vector<std::unique_ptr<grpc::Server>> servers;
+  std::vector<std::string> addr_v = {"0.0.0.0:50041", "0.0.0.0:50042", "0.0.0.0:50043",  "0.0.0.0:50044", "0.0.0.0:50045"};
+  std::vector<std::shared_ptr<grpc::Channel>> channels = make_channels(replica_size, addr_v);
+  
+  for (int i = 0; i < replica_size; ++i) {
+    std::cout << i << " start" << std::endl;
+    std::unique_ptr<PaxosServiceImpl> paxos_service = std::unique_ptr<PaxosServiceImpl>(
+      new PaxosServiceImpl(replica_size,channels, i)
+    );
+    std::unique_ptr<grpc::Server> paxos_server = initialize_service(addr_v[i], paxos_service.get());
+    std::thread paxos_thread(start_service, paxos_server.get()); // If not stored, paxos_server will be destructed every time after the loop
+    pxs_threads.push_back(std::move(paxos_thread));
+    pxs.push_back(std::move(paxos_service));
+    servers.push_back(std::move(paxos_server));
+    std::cout << i << " ok" << std::endl;
+  }
+
+
+
+  grpc::Status put_status = pxs.at(1)->Start(1,"hi"); // use at as a reference
+  usleep(5000000);
+  put_status = pxs.at(2)->Start(2, "2");
+
+  pxs.at(1)->Start(3,"3");
+  pxs.at(1)->Start(4,"4");
+  assertEqual(pxs[0].get(),pxs[1].get(),4);
+  assertEqual(pxs[1].get(),pxs[2].get(),4);
+  assertEqual(pxs[2].get(),pxs[3].get(),4);
+  assertEqual(pxs[3].get(),pxs[4].get(),4);
+  std::cout << "pass test befor a few server die" << std::endl;
+
+  // make a majority of server die
+  std::set<int> deadServer = {2,3,4};
+  for (auto i : deadServer) {
+    shutdown_service(servers.at(i).get());
+  }
+  std::cout << "server 2 and server 3 and server 4 is down" << std::endl;
+
+  pxs.at(0)->Start(5,"5");
+  pxs.at(1)->Start(6,"6");
+  assertFail(pxs[0].get(),pxs[1].get(),6);
+  assertFail(pxs[1].get(),pxs[2].get(),6);
+  std::cout << "minority servers cannot make any progress" << std::endl;
+  // shut down alive the service
+  for (int i=0; i<replica_size; i++) {
+    if (deadServer.count(i)==0)
+      shutdown_service(servers.at(i).get());
+  }
+
+
+
+  // join all the threads
+  for (int i = 0; i < replica_size; i++) {
+    if (pxs_threads[i].joinable()) {
+      pxs_threads[i].join();
+      std::cout << i << " joined" << std::endl;
+    }
+  }
+}
+
+void test_concurrent() {
+  int replica_size = 3;
+  std::vector<std::unique_ptr<PaxosServiceImpl>> pxs;
+  std::vector<std::thread> pxs_threads;
+  std::vector<std::unique_ptr<grpc::Server>> servers;
+  std::vector<std::string> addr_v = {"0.0.0.0:50041", "0.0.0.0:50042", "0.0.0.0:50043"};
+  std::vector<std::shared_ptr<grpc::Channel>> channels = make_channels(replica_size, addr_v);
+  
+  for (int i = 0; i < replica_size; ++i) {
+    std::cout << i << " start" << std::endl;
+    std::unique_ptr<PaxosServiceImpl> paxos_service = std::unique_ptr<PaxosServiceImpl>(
+      new PaxosServiceImpl(replica_size,channels, i)
+    );
+    std::unique_ptr<grpc::Server> paxos_server = initialize_service(addr_v[i], paxos_service.get());
+    std::thread paxos_thread(start_service, paxos_server.get()); // If not stored, paxos_server will be destructed every time after the loop
+    pxs_threads.push_back(std::move(paxos_thread));
+    pxs.push_back(std::move(paxos_service));
+    servers.push_back(std::move(paxos_server));
+    std::cout << i << " ok" << std::endl;
+  }
+
+
+
+  pxs.at(1)->Start(1,"hi"); 
+  pxs.at(2)->Start(1, "2");
+  pxs.at(0)->Start(1,"hello");
+
+  pxs.at(1)->Start(2,"hi2"); 
+  pxs.at(2)->Start(2, "22");
+  pxs.at(0)->Start(2,"hello2");
+
+  pxs.at(1)->Start(1,"1");
+
+
+  assertEqual(pxs[0].get(),pxs[1].get(),2);
+  assertEqual(pxs[1].get(),pxs[2].get(),2);
+  std::cout << "pass concurrent test" << std::endl;
+
+  // shut down alive the service
+  for (int i=0; i<replica_size; i++) {
+    shutdown_service(servers.at(i).get());
+  }
+
+  // join all the threads
+  for (int i = 0; i < replica_size; i++) {
+    if (pxs_threads[i].joinable()) {
+      pxs_threads[i].join();
+      std::cout << i << " joined" << std::endl;
+    }
+  }
+}
+
+
 int main(int argc, char** argv) {
 
   test_basic_put();
-
+  test_unreliable();
+  test_minority();
+  test_concurrent();
 
   // ----------------------  auto/loop creation of the paxos service----------------------
   // make_paxos_services(replica_size, addr_v, channels);
-
 
 }
