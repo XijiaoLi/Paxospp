71ca2757f55487c00f3faa2c9b96b07d023ee8f1
XijiaoLi <31825116+XijiaoLi@users.noreply.github.com>
2021-04-08 22:35:31 -0400

    Update paxos.h

--- include/paxos.h
+++ include/paxos.h
@@ -49,20 +49,16 @@ class PaxosServiceImpl final : public Paxos::Service {
   
   public:
     PaxosServiceImpl(int peers_num, std::vector<std::string> peers_addr, int me);
-    PaxosServiceImpl(int peers_num, std::vector<std::string> peers_addr, int me, bool debug);
-
+    // Paxos Ping service test if the server is available
+    grpc::Status Ping(ServerContext* context, const EmptyMessage* request, EmptyMessage* response) override;
+    // Paxos Receice service to receive proposals
+    grpc::Status Receive(ServerContext* context, const Proposal* proposal, Response* response) override;
     // Initialize server, channel, stub
     void InitializeService();
     // Start listening on the address
     void StartService();
     // Shut down the service on the server
     void TerminateService();
-
-    // Paxos Ping service test if the server is available
-    grpc::Status Ping(ServerContext* context, const EmptyMessage* request, EmptyMessage* response) override;
-    // Paxos Receice service to receive proposals
-    grpc::Status Receive(ServerContext* context, const Proposal* proposal, Response* response) override;
-    
     // Main entry point for running Paxos service
     grpc::Status Start(int seq, std::string v);
     // Check a paxos peer's decision on an instance
