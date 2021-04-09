79e490911fa7c5fe713dde2d2a70bda1aa7c8a08
XijiaoLi <31825116+XijiaoLi@users.noreply.github.com>
2021-04-09 00:09:36 -0400

    Update kvclient.h

--- example/kvstore/kvclient.h
+++ example/kvstore/kvclient.h
@@ -1,2 +1,16 @@
 #include <unistd.h>
 #include "kvstore.h"
+
+class KVStoreClient
+{
+  public:
+    KVStoreClient(std::shared_ptr<grpc::Channel> channel)
+      : stub(KVStore::NewStub(channel)) {}
+
+    std::string Put(const std::string& key, const std::string& value);
+
+    std::string Get(const std::string& key, const std::string& val);
+
+  private:
+    std::unique_ptr<KVStore::Stub> stub;
+};
